0241f81efb486bd56e4b9ac50a8e3e741377a383
Vanessa Burroughs <vburroughs21@gmail.com>
2019-08-06 17:29:26 -0500

    OOP Review session

---
+++ mod_1/cheatsheet.md
@@ -0,0 +1,21 @@
+### Instance variables vs Class variables
+
+- class = same for every member(instance of the class). Applies to everyone(all instances) when changed.
+  `ex: @@bank_name = "Chase"`
+- instance = different for every member (instance of the class)
+  `ex: user_id = "9845443645" balance: "1000"`
+
+
+### Initialize method
+
+Runs everytime a new instance of a class is created.
+Define all variables in initalize!
+
+### Self
+Self is the object that the method is called on
+
+`@@all  << self`
+Refers to the instance that has just been created, the current instance
+
+`def self.get_all_bank_accounts()`
+Refers to the `BankAccount` class
--- mod_1/intro-oo/account.rb
+++ mod_1/intro-oo/account.rb
@@ -1,30 +1,57 @@
+# Creating a  class called BankAccount
 class BankAccount
+    
+    # attr_accessor creates getter and setter methods for user_id and balance
     attr_accessor :user_id, :balance
+    # Creates the class variable all 
     @@all = []
+    @@bank_name = "Chase"
+
+    # Defining the initalize method. Runs when a new instance is created
     def initialize(user_id, balance)
+        # Setting the user_id instance variable to the first value passed in when a bank account is created
         @user_id = user_id
+        # Setting the balance instance variable to the second value passed in when a bank account is created
         @balance = balance
+        @username = ""
+
+        # Pushes the newly created instance of bank account into the class variable all
         @@all  << self
     end
 
-    def self.get_self()
+    # Defining class method get_all_bank_accounts
+    def self.get_all_bank_accounts()
+        # Returning the bank account class varaible [{account1}, {account2}]
         @@all
     end
     def deposit(amount)
+        # Adding the `amount` to balance
         @balance += amount
     end
     def withdraw(amount)
+        # Subtract `amount` from balance
         @balance -= amount
+
+        # Returns current instance that called withdrawl
+        puts self.user_id
     end
 end
 
 ba_sf = BankAccount.new(3,10000)
 ba_atx = BankAccount.new(4, 50000)
 ba_dc = BankAccount.new(5, 60000)
-richies =  BankAccount.get_self.each{
-     |ba| 
-     ba.balance += 10000
- }
- richies.each {
-     |richies| puts richies.balance
- }
+
+
+ba_sf.withdraw(100)
+
+
+richies =  BankAccount.get_all_bank_accounts
+# richies =  BankAccount.get_all_bank_accounts.each{
+#      |ba| 
+#      ba.balance += 10000
+#  }
+#  richies.each {
+#      |richies| puts richies.balance
+#  }
+
+# puts richies
