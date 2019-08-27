bc01c554673d405a3dad6f7cc0e850c13524a2d2
Vanessa Burroughs <vburroughs21@gmail.com>
2019-08-27 09:41:55 -0500

    Add sinatra lecture code

--- mod_2/sinatra-mvc-intro/app/controllers/application_controller.rb
+++ mod_2/sinatra-mvc-intro/app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < Sinatra::Base
+  get '/' do
 
+    erb :index
+  end
+
+  get '/users' do
+    "Hello, World!"
+  end
 end
+
+
+# Steps 
+# 1: Type link in browser
+# 2: Server recieved a get request to '/'
+# 3/4: Controller returned index.erb
+# 5: Response to client with page
