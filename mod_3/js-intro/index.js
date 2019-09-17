6fce4921dfce6d55a8b8cc2306e2c6f90f023e82
Vanessa Burroughs <vburroughs21@gmail.com>
2019-09-17 10:05:43 -0500

    Added events intro starter code

--- mod_3/js-intro/index.js
+++ mod_3/js-intro/index.js
@@ -1,3 +1,7 @@
 console.log('%c HELLO WORLD', 'color: green')
 console.log('%c HELLO WORLD', 'color: gold')
 console.log('%c HELLO WORLD', 'color: firebrick')
+
+var name = "vanessa";
+
+console.log(name)
