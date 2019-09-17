e8c96d599a92f0bef8352215be32b35cd29462dd
Vanessa Burroughs <vburroughs21@gmail.com>
2019-09-17 13:28:55 -0500

    Added lecture notes

--- mod_3/events-intro/src/index.js
+++ mod_3/events-intro/src/index.js
@@ -1,17 +1,20 @@
-// 
+// Adds an event listener to the document that waits 
+// for the entire page to load then runs the code
 document.addEventListener('DOMContentLoaded', function() {
 
-  // 
+  // waits for a click on the document then runs the function
   document.addEventListener("click", (event) => {
+    
+    // event is the mouse click event
+    // event.target is the element that was clicked on
     console.log(event.target)
-    // event.preventDefault();
-    // console.log(event.target);
+
+    // Removes the element that was clicked on
     event.target.remove();
   });
 
+  // saves the element with the id `comment-form` to the cForm variable
   var cForm = document.getElementById("comment-form");
-
-  console.log(cForm);
 
 
   // var vs let vs const
